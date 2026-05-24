2f685125cdf28a1bc77deeb7ae197d48e585f962
Jahnavi-1511 <jahnavi15cherukuri@gmail.com>
2026-05-24 22:00:22 +0530

    Delete devlearnhub-nextjs directory

--- devlearnhub-nextjs/app/tutorials/page.js
+++
@@ -1,4 +0,0 @@
-export default function Tutorials(){
-const data=['HTML','CSS','JavaScript','Angular','React','.NET','SQL']
-return <div style={{padding:'40px'}}><h1>Tutorials</h1>{data.map(i=><div key={i} style={{margin:'20px 0',padding:'20px',background:'#111827'}}>{i} Tutorial</div>)}</div>
-}
