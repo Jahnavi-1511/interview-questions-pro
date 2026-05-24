c84ffb766e465fd1124024affd02e29d42972668
Jahnavi-1511 <jahnavi15cherukuri@gmail.com>
2026-05-24 21:55:55 +0530

    Delete app directory

--- app/blog/page.js
+++
@@ -1,4 +0,0 @@
-export default function Blog(){
-const data=['Frontend Roadmap','Angular Signals','React Hooks','RxJS Guide']
-return <div style={{padding:'40px'}}><h1>Developer Blogs</h1>{data.map(i=><div key={i} style={{margin:'20px 0',padding:'20px',background:'#111827'}}>{i}</div>)}</div>
-}
